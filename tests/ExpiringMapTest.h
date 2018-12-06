e5a9f9ac157bcaceba2597ce8cab7da94b204d82
artur00231 <29777966+artur00231@users.noreply.github.com>
2018-12-06 12:21:08 +0100

    Test code improvment

--- tests/ExpiringMapTest.h
+++ tests/ExpiringMapTest.h
@@ -8,31 +8,31 @@ TEST(ExpiringMap, insert)
 {
 	using namespace std::chrono_literals;
 
-	ExpiringMap<int, int> db{ 10h };
+	ExpiringMap<int, int> expiring_map{ 10h };
 
-	db.insert(10, 45);
+	expiring_map.insert(10, 45);
 
-	auto x = db.get(10);
+	auto x = expiring_map.get(10);
 
 	EXPECT_EQ(x.has_value(), true);
 	EXPECT_EQ(x.value(), 45);
 
-	db.insert(20, 5);
-	db.insert(5, 20);
+	expiring_map.insert(20, 5);
+	expiring_map.insert(5, 20);
 
-	x = db.get(5);
+	x = expiring_map.get(5);
 
 	EXPECT_EQ(x.has_value(), true);
 	EXPECT_EQ(x.value(), 20);
 
-	x = db.get(20);
+	x = expiring_map.get(20);
 
 	EXPECT_EQ(x.has_value(), true);
 	EXPECT_EQ(x.value(), 5);
 
-	db.insert(10, 0);
+	expiring_map.insert(10, 0);
 
-	x = db.get(10);
+	x = expiring_map.get(10);
 
 	EXPECT_EQ(x.has_value(), true);
 	EXPECT_EQ(x.value(), 0);
@@ -42,11 +42,11 @@ TEST(ExpiringMap, emplace)
 {
 	using namespace std::chrono_literals;
 
-	ExpiringMap<int, std::pair<int, int>> db{ 10h };
+	ExpiringMap<int, std::pair<int, int>> expiring_map{ 10h };
 
-	db.emplace(10, 45, 30);
+	expiring_map.emplace(10, 45, 30);
 
-	auto x = db.get(10);
+	auto x = expiring_map.get(10);
 
 	EXPECT_EQ(x.has_value(), true);
 	EXPECT_EQ(x.value(), std::make_pair(45, 30));
@@ -56,24 +56,24 @@ TEST(ExpiringMap, assign)
 {
 	using namespace std::chrono_literals;
 
-	ExpiringMap<int, int> db{ 10h };
+	ExpiringMap<int, int> expiring_map{ 10h };
 
-	db.insert(10, 45);
+	expiring_map.insert(10, 45);
 
-	auto x = db.get(10);
+	auto x = expiring_map.get(10);
 
 	EXPECT_EQ(x.has_value(), true);
 	EXPECT_EQ(x.value(), 45);
 
-	auto success = db.assign(10, 10);
+	auto success = expiring_map.assign(10, 10);
 
-	x = db.get(10);
+	x = expiring_map.get(10);
 
 	EXPECT_EQ(x.has_value(), true);
 	EXPECT_EQ(success, true);
 	EXPECT_EQ(x.value(), 10);
 
-	success = db.assign(20, 5);
+	success = expiring_map.assign(20, 5);
 
 	EXPECT_EQ(success, false);
 }
@@ -82,19 +82,19 @@ TEST(ExpiringMap, assign_emplace)
 {
 	using namespace std::chrono_literals;
 
-	ExpiringMap<int, std::pair<int, int>> db{ 10h };
+	ExpiringMap<int, std::pair<int, int>> expiring_map{ 10h };
 
-	db.insert(10, std::make_pair(1, 1));
+	expiring_map.insert(10, std::make_pair(1, 1));
 
-	auto success = db.assign_emplace(10, 45, 30);
+	auto success = expiring_map.assign_emplace(10, 45, 30);
 
-	auto x = db.get(10);
+	auto x = expiring_map.get(10);
 
 	EXPECT_EQ(x.has_value(), true);
 	EXPECT_EQ(success, true);
 	EXPECT_EQ(x.value(), std::make_pair(45, 30));
 
-	success = db.assign_emplace(20, 45, 30);
+	success = expiring_map.assign_emplace(20, 45, 30);
 
 	EXPECT_EQ(success, false);
 }
@@ -103,16 +103,16 @@ TEST(ExpiringMap, get)
 {
 	using namespace std::chrono_literals;
 
-	ExpiringMap<int, int> db{ 10h };
+	ExpiringMap<int, int> expiring_map{ 10h };
 
-	db.insert(10, 45);
+	expiring_map.insert(10, 45);
 
-	auto x = db.get(10);
+	auto x = expiring_map.get(10);
 
 	EXPECT_EQ(x.has_value(), true);
 	EXPECT_EQ(x.value(), 45);
 
-	x = db.get(25);
+	x = expiring_map.get(25);
 
 	EXPECT_EQ(x.has_value(), false);
 }
@@ -121,17 +121,17 @@ TEST(ExpiringMap, at1)
 {
 	using namespace std::chrono_literals;
 
-	ExpiringMap<int, int> db{ 10h };
+	ExpiringMap<int, int> expiring_map{ 10h };
 
-	db.insert(10, 45);
+	expiring_map.insert(10, 45);
 
-	auto x = db.at(10);
+	auto x = expiring_map.at(10);
 	EXPECT_EQ(x, 45);
 
-	EXPECT_THROW(db.at(25), std::out_of_range);
+	EXPECT_THROW(expiring_map.at(25), std::out_of_range);
 
 	try {
-		x = db.at(25);
+		x = expiring_map.at(25);
 	}
 	catch (const std::out_of_range & out)
 	{
@@ -143,17 +143,17 @@ TEST(ExpiringMap, at2)
 {
 	using namespace std::chrono_literals;
 
-	ExpiringMap<int, int> db{ 10h };
+	ExpiringMap<int, int> expiring_map{ 10h };
 
-	db.insert(10, 45);
+	expiring_map.insert(10, 45);
 
-	auto x = db[10];
+	auto x = expiring_map[10];
 	EXPECT_EQ(x, 45);
 
-	EXPECT_THROW(db[25], std::out_of_range);
+	EXPECT_THROW(expiring_map[25], std::out_of_range);
 
 	try {
-		x = db[25];
+		x = expiring_map[25];
 	}
 	catch (const std::out_of_range & out)
 	{
@@ -165,33 +165,33 @@ TEST(ExpiringMap, erase)
 {
 	using namespace std::chrono_literals;
 
-	ExpiringMap<int, int> db{ 10h };
+	ExpiringMap<int, int> expiring_map{ 10h };
 
-	db.insert(10, 45);
+	expiring_map.insert(10, 45);
 
-	auto x = db[10];
+	auto x = expiring_map[10];
 	EXPECT_EQ(x, 45);
 
-	db.erase(10);
+	expiring_map.erase(10);
 
-	EXPECT_THROW(db[10], std::out_of_range);
+	EXPECT_THROW(expiring_map[10], std::out_of_range);
 }
 
 TEST(ExpiringMap, data_live_time1)
 {
 	using namespace std::chrono_literals;
 
-	ExpiringMap<int, int> db{ 2ms };
+	ExpiringMap<int, int> expiring_map{ 2ms };
 
-	db.insert(10, 45);
+	expiring_map.insert(10, 45);
 
-	auto x = db.get(10);
+	auto x = expiring_map.get(10);
 
 	EXPECT_EQ(x.has_value(), true);
 
 	std::this_thread::sleep_for(2ms);
 
-	x = db.get(10);
+	x = expiring_map.get(10);
 
 	EXPECT_EQ(x.has_value(), false);
 }
@@ -200,48 +200,48 @@ TEST(ExpiringMap, data_live_time2)
 {
 	using namespace std::chrono_literals;
 
-	ExpiringMap<int, int> db{ 2ms };
+	ExpiringMap<int, int> expiring_map{ 2ms };
 
-	db.insert(10, 45);
-	db.emplace(11, 45);
-	db.insert(12, 0);
-	db.assign(12, 45);
-	db.insert(13, 0);
-	db.assign_emplace(13, 45);
-	db.insert(20, 45);
-	db.insert(30, 45);
+	expiring_map.insert(10, 45);
+	expiring_map.emplace(11, 45);
+	expiring_map.insert(12, 0);
+	expiring_map.assign(12, 45);
+	expiring_map.insert(13, 0);
+	expiring_map.assign_emplace(13, 45);
+	expiring_map.insert(20, 45);
+	expiring_map.insert(30, 45);
 
-	auto x = db.get(10);
+	auto x = expiring_map.get(10);
 	EXPECT_EQ(x.has_value(), true);
-	x = db.get(11);
+	x = expiring_map.get(11);
 	EXPECT_EQ(x.has_value(), true);
-	x = db.get(12);
+	x = expiring_map.get(12);
 	EXPECT_EQ(x.has_value(), true);
-	x = db.get(13);
+	x = expiring_map.get(13);
 	EXPECT_EQ(x.has_value(), true);
 
-	auto old_duration = db.getDataLiveTime();
-	db.setDataLiveTime(1h);
-	x = db.get(20);
-	db.setDataLiveTime(old_duration);
+	auto old_duration = expiring_map.getDataLiveTime();
+	expiring_map.setDataLiveTime(1h);
+	x = expiring_map.get(20);
+	expiring_map.setDataLiveTime(old_duration);
 
 	EXPECT_EQ(x.has_value(), true);
-	x = db.get(30);
+	x = expiring_map.get(30);
 	EXPECT_EQ(x.has_value(), true);
 
 	std::this_thread::sleep_for(2ms);
 
-	x = db.get(10);
+	x = expiring_map.get(10);
 	EXPECT_EQ(x.has_value(), false);
-	x = db.get(11);
+	x = expiring_map.get(11);
 	EXPECT_EQ(x.has_value(), false);
-	x = db.get(12);
+	x = expiring_map.get(12);
 	EXPECT_EQ(x.has_value(), false);
-	x = db.get(13);
+	x = expiring_map.get(13);
 	EXPECT_EQ(x.has_value(), false);
-	x = db.get(20);
+	x = expiring_map.get(20);
 	EXPECT_EQ(x.has_value(), true);
-	x = db.get(30);
+	x = expiring_map.get(30);
 	EXPECT_EQ(x.has_value(), false);
 }
 
@@ -249,18 +249,18 @@ TEST(ExpiringMap, clear_size_empty)
 {
 	using namespace std::chrono_literals;
 
-	ExpiringMap<int, int> db{ 2h };
+	ExpiringMap<int, int> expiring_map{ 2h };
 
-	db.insert(10, 45);
-	db.insert(20, 45);
-	db.insert(30, 45);
-	db.insert(40, 45);
-	db.insert(50, 45);
+	expiring_map.insert(10, 45);
+	expiring_map.insert(20, 45);
+	expiring_map.insert(30, 45);
+	expiring_map.insert(40, 45);
+	expiring_map.insert(50, 45);
 
-	EXPECT_EQ(db.size(), 5);
+	EXPECT_EQ(expiring_map.size(), 5);
 
-	db.clear();
+	expiring_map.clear();
 
-	EXPECT_EQ(db.size(), 0);
-	EXPECT_EQ(db.empty(), true);
+	EXPECT_EQ(expiring_map.size(), 0);
+	EXPECT_EQ(expiring_map.empty(), true);
 }
